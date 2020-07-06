916eb616ead0c997eed389973d4e423a439ce3ba
Ajeet Kumar <ajeet.ajeetkumar@gmail.com>
2020-07-06 09:10:05 +0530

    Initial Commit

---
+++ Calculator SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator SwiftUI
+//
+//  Created by Ajeet Kumar on 06/07/20.
+//  Copyright © 2020 DigitalCurry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
